595dd78358ed53dc2e7be2c41a5ec07babc8aacc
Yanuar <45526227+Yanuarramadhan@users.noreply.github.com>
2018-12-13 17:00:23 +0700

    codingya

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+int n;
+cout << "Masukkan nilai n: "<<endl;
+cin>> n;
+int i;
+  for(i=1;i<=n;i++)
+cout <<("Algoritma dan Pemrograman") <<endl;
+
+}
